bc1cf178e693228119cc13fb0caca13d0e24642b
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2022-11-07 07:54:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const UserDetails = require("./models/userDetails");
 const fileUpload = require("express-fileupload");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const database_url =
   "mongodb+srv://admin:admin@cluster0.ixydjpe.mongodb.net/test";
